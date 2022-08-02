475988f239f2ceb9ee8698563c3b44d5b3a71462
Saish Menon <saishmenon001@gmail.com>
2022-08-01 17:44:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='flex justify-center items-center mt-6'>
+      <h1 className="text-3xl">
+        Hello world!
+      </h1>
+   </div>
   );
 }
 
