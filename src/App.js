ac2db9c665d5927482740f33a59273feca744cf4
Saish Menon <saishmenon001@gmail.com>
2022-08-03 18:02:46 -0700

    Update Styles and Template Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import './App.css';
 
-function App() {
+export default function App() {
   return (
-   <div className='flex justify-center items-center mt-6'>
-      <h1 className="text-3xl">
-        Hello world!
-      </h1>
+   <div className='flex h-screen flex-col justify-center items-center mt-6'>
+      <h1>Hello!</h1>
+      <p>This is a <a href="https://reactjs.org/" className="text-blue-500 font-medium">React.js</a> and <a href="https://tailwindcss.com/" className="text-cyan-500 font-medium">TailwindCSS</a> starter project.</p>
    </div>
   );
 }
-
-export default App;
